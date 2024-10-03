e93bd79a4dcf78ea0d2966b8de6c76f02c6ea63e
Alireza Ghanbari <91381302+alirezza96@users.noreply.github.com>
2024-10-03 11:56:05 +0330

    modified mustRegistered middleware

--- middlewares/mustAdmin.js
+++ middlewares/mustAdmin.js
@@ -1,9 +1,8 @@
 export default async function mustAdmin(req, res, next) {
-    const { is_admin } = req.body
-    if (req.url.startsWith("/api/podcasts")) {
-
-        if (is_admin) return res.status(403).json({ message: "access denied" })
-    }
+    const is_admin = true
+    // if (req.url.startsWith("/api/podcasts")) {
+    if (!is_admin) return res.status(403).json({ message: "access denied! call System Admin" })
+    // }
     next()
 }
 
